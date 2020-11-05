e77889b095b00e9aee9e1a9fd34f17bee2667a9e
lucadolphin <luca.inglese@gmail.com>
2020-11-05 09:31:02 +0100

    fixed stage link

--- libs/config.stage.js
+++ libs/config.stage.js
@@ -1,4 +1,4 @@
 export const EXENTRIQ_AVATAR_URL = 'https://stage.exentriq.com/AvatarService?username=';
 export const EXENTRIQ_TALK_URL = 'https://talk-stage.exentriq.com';
-export const EXENTRIQ_EMA_URL = 'http://boards-new.stage.exentriq.com';
+export const EXENTRIQ_EMA_URL = 'https://boards-new-stage.exentriq.com';
 export const EXENTRIQ_URL_HOST = 'https://stage.exentriq.com/JSON-RPC';
