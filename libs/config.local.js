669a9579dae7b8a29626b48b282bff0c6cd4bc6c
lucadolphin <luca.inglese@gmail.com>
2021-01-20 11:24:14 +0100

    added config for Dindle app

--- libs/config.local.js
+++ libs/config.local.js
@@ -2,3 +2,5 @@ export const EXENTRIQ_AVATAR_URL = 'https://stage.exentriq.com/AvatarService?use
 export const EXENTRIQ_TALK_URL = 'http://192.168.1.132:3000';
 export const EXENTRIQ_EMA_URL = 'http://192.168.1.132:3002';
 export const EXENTRIQ_URL_HOST = 'https://stage.exentriq.com/JSON-RPC';
+export const DINDLEAPP = 'https//dindle0.exentriqlab.com';
+
--- libs/config.stage.js
+++ libs/config.stage.js
@@ -2,3 +2,5 @@ export const EXENTRIQ_AVATAR_URL = 'https://stage.exentriq.com/AvatarService?use
 export const EXENTRIQ_TALK_URL = 'https://talk-stage.exentriq.com';
 export const EXENTRIQ_EMA_URL = 'https://boards-new-stage.exentriq.com';
 export const EXENTRIQ_URL_HOST = 'https://stage.exentriq.com/JSON-RPC';
+export const DINDLEAPP = 'https//dindle0.exentriqlab.com';
+
