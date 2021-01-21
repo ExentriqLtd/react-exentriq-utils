ad9aea765d7236cc1a71646d9a29a755aad46db1
lucadolphin <luca.inglese@gmail.com>
2021-01-21 15:03:27 +0100

    fixed dindle link

--- libs/config.prod.js
+++ libs/config.prod.js
@@ -2,4 +2,4 @@ export const EXENTRIQ_AVATAR_URL = 'https://www.exentriq.com/AvatarService?usern
 export const EXENTRIQ_TALK_URL = 'https://talk.exentriq.com';
 export const EXENTRIQ_EMA_URL = 'https://boards-new.exentriq.com';
 export const EXENTRIQ_URL_HOST = 'https://www.exentriq.com/JSON-RPC';
-export const DINDLEAPP = 'https//www.dindle.com';
+export const DINDLEAPP = 'https://www.dindle.com';
