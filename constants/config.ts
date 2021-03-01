c9029c1419e6f4f8d20a297252e7486e211cef57
lucadolphin <luca.inglese@gmail.com>
2021-03-01 09:59:38 +0100

    changed link on prod

--- constants/config.ts
+++ constants/config.ts
@@ -1,3 +1,3 @@
-export const URL_DEFAULT_TALK = "https://talk-stage.exentriq.com";
-export const URL_EXENTRIQ_HOST = "https://stage.exentriq.com/JSON-RPC";
-export const METHOD_AUTH_GUARDIAN_LOGIN = "auth.login";
+export const URL_DEFAULT_TALK = 'https://talk.exentriq.com';
+export const URL_EXENTRIQ_HOST = 'https://www.exentriq.com/JSON-RPC';
+export const METHOD_AUTH_GUARDIAN_LOGIN = 'auth.login';
