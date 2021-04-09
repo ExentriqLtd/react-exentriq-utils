c4afd4203985f346ba1bc6f235c97bfab4080aa7
lucadolphin <luca.inglese@gmail.com>
2021-04-09 18:38:05 +0200

    fixed ordered rooms

--- functions/orderRoomsByDate.ts
+++ functions/orderRoomsByDate.ts
@@ -3,6 +3,23 @@
 import { TRoom } from "../declarations/rooms";
 import memoize from "fast-memoize";
 
+interface TDate {
+  $date: number;
+}
+
+const getDate = (field: TRoom): number => {
+  const exUpdatedAt = Object.prototype.hasOwnProperty.call(field, 'exUpdatedAt');
+  const lm = Object.prototype.hasOwnProperty.call(field, 'lm');
+  const tlv = Object.prototype.hasOwnProperty.call(field, 'tlv');
+  const ls = Object.prototype.hasOwnProperty.call(field, 'ls');
+  const ts = Object.prototype.hasOwnProperty.call(field, 'ts');
+  if (exUpdatedAt) return field.exUpdatedAt.$date;
+  if (lm) return field.lm.$date;
+  if (ls) return field.ts.$date;
+  if (tlv) return field.tlv.$date;
+  return field.ts.$date;
+};
+
 export const utilityOrderRoomsByDate = memoize((rooms: TRoom[]): TRoom[] =>
-  rooms.slice().sort((a: TRoom, b: TRoom): number => b.lm.$date - a.lm.$date)
+  rooms.slice().sort((a: TRoom, b: TRoom): number => getDate(b) - getDate(a)),
 );
