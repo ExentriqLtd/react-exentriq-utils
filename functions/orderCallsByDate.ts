37cabfe032c4cefda4ce4082cdfdad64ffd25f87
Fabrizio Corpora <fabrizio.corpora@gmail.com>
2021-09-16 12:55:10 +0200

    hot fix order calls by date and remove entity calls

--- functions/orderCallsByDate.ts
+++ functions/orderCallsByDate.ts
@@ -1,18 +1,22 @@
 /** @format */
 
-import { TCalls } from "../declarations/calls";
+import { TRoom } from "../declarations/rooms";
 import memoize from "fast-memoize";
 
-interface TDate {
-  $date: number;
-}
-
-const getDateCalls = (field: TCalls): number => {
-  const started = Object.prototype.hasOwnProperty.call(field, 'started');
-  if (started) return field.started.$date;
+const getDateCalls = (field: TRoom): number => {
+  const exUpdatedAt = Object.prototype.hasOwnProperty.call(field, 'exUpdatedAt');
+  const lm = Object.prototype.hasOwnProperty.call(field, 'lm');
+  const tlv = Object.prototype.hasOwnProperty.call(field, 'tlv');
+  const ls = Object.prototype.hasOwnProperty.call(field, 'ls');
+  const ts = Object.prototype.hasOwnProperty.call(field, 'ts');
+  if (lm) return field.lm.$date;
+  if (tlv) return field.tlv.$date;
+  if (ls) return field.ts.$date;
+  if (ts) return field.ts.$date;
+  if (exUpdatedAt) return field.exUpdatedAt.$date;
   return 0;
 };
 
-export const utilityOrderCallsByDate = memoize((calls: TCalls[]): TCalls[] =>
-  calls.slice().sort((a: TCalls, b: TCalls): number => getDateCalls(b) - getDateCalls(a)),
+export const utilityOrderCallsByDate = memoize((calls: TRoom[]): TRoom[] =>
+  calls.slice().sort((a: TRoom, b: TRoom): number => getDateCalls(b) - getDateCalls(a)),
 );
