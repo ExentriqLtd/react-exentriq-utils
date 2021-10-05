e20637a7727dd0f7e51f558b6f4c8f021d1e04e3
Larens94 <fabrizio.corpora@gmail.com>
2021-10-06 01:17:39 +0200

    edit getAddressNameByCordinates promise

--- functions/getAddressLocation.ts
+++ functions/getAddressLocation.ts
@@ -1,14 +1,15 @@
 import { GOOGLE_PLACES_API_KEY } from "../libs/config";
 
-
-export const getAddressNameByCordinates = async (geoLocation: any) => {
+export const getAddressNameByCordinates = async (geoLocation: any,callback) => {
   const latitude = geoLocation.latitude.toFixed(7);
   const longitude = geoLocation.longitude.toFixed(7);
-  let res = [];
-  const response  = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_PLACES_API_KEY}`);
-
-  const json = await response.json();
-  res = json.results; 
-
-  return res;
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_PLACES_API_KEY}`)
+  .then((response)=>response.json())
+  .then((responseData) => {
+    callback(responseData.results);
+  })
+  .catch(error => {
+    console.warn("getAddressNameByCordinates::error",error)
+    callback([]);
+  });
 };
