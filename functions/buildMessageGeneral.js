30c1e8c690abfe4ba5cb04c461c5911b2413ffcb
Fabrizio Corpora <fabrizio.corpora@gmail.com>
2021-12-29 18:44:55 +0100

    fix normal input (search is disabled)

--- functions/buildMessageGeneral.js
+++ functions/buildMessageGeneral.js
@@ -8,7 +8,7 @@ import { Text } from 'react-native-paper';
  * @param active
  * @returns
  */
-export const buildMessageGeneral = ({ message, styles, active = true, useSeparatorFixCursive = true }) => {
+export const buildMessageGeneral = ({ message, styles, active = true }) => {
   let outputArr = [];
   let outputArrString = [];
   let i = 0;
@@ -16,42 +16,41 @@ export const buildMessageGeneral = ({ message, styles, active = true, useSeparat
   let openSelected = false;
   let tmpMention = '';
   let separator = { open:'"', close:'"'};
-  if (useSeparatorFixCursive) {
-  }
+
   if (text){
     while (i < text.length) {
-       //open search
-       if (!openSelected && text[i] === '@' && !text[i + 1] && text[i + 1] !== separator.open){
-        outputArr.push(text[i], separator.open);
-        outputArrString.push(text[i],separator.open);
-      }
-
-      //normal
-      if (!openSelected && text[i] !== '@' && text[i + 1] !== separator.open) {
+      if (active){
+        //open search
+        if (!openSelected && text[i] === '@' && !text[i + 1] && text[i + 1] !== separator.open){
+          outputArr.push(text[i], separator.open);
+          outputArrString.push(text[i],separator.open);
+        }
+        //normal
+        if (!openSelected && text[i] !== '@' && text[i + 1] !== separator.open) {
+          outputArr.push(text[i]);
+          outputArrString.push(text[i]);
+        }
+        if (openSelected) {
+          tmpMention = tmpMention + text[i];
+          if (text[i] ===  separator.close && text[i - 1] !== '@') {
+            openSelected = false;
+            // const mention = '@“(<Text key={i} style={styles.mentionText}>'+tmpMention+'</Text>“';
+            let mention = (
+              <Text key={i} style={styles.mentionText}>
+                {'@' + tmpMention}
+              </Text>
+            );
+            outputArr.push(mention);
+            outputArrString.push('@' + tmpMention);
+            tmpMention = '';
+          }
+        }
+        if (text[i] === '@' && text[i + 1] === separator.open) {
+          openSelected = true;
+        }
+      } else {
         outputArr.push(text[i]);
         outputArrString.push(text[i]);
-      }
-
-      if (openSelected) {
-        tmpMention = tmpMention + text[i];
-        if (text[i] ===  separator.close && text[i - 1] !== '@') {
-          openSelected = false;
-          // const mention = '@“(<Text key={i} style={styles.mentionText}>'+tmpMention+'</Text>“';
-          let mention = (
-            <Text key={i} style={styles.mentionText}>
-              {'@' + tmpMention}
-            </Text>
-          );
-          if (!active){
-            mention = tmpMention;
-          }
-          outputArr.push(mention);
-          outputArrString.push('@' + tmpMention);
-          tmpMention = '';
-        }
-      }
-      if (text[i] === '@' && text[i + 1] === separator.open) {
-        openSelected = true;
       }
       i++;
     }
