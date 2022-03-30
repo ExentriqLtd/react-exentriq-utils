62c9aab7da71b4f5524865544cdfc4f10eb35639
Larens94 <fabrizio.corpora@gmail.com>
2022-03-30 12:16:36 +0200

    fix for talk item

--- components/AvatarInitials/AvatarInitials.js
+++ components/AvatarInitials/AvatarInitials.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { View, StyleSheet, Text} from 'react-native';
+import { APP_NAME } from '../../constants/config'
 
 function AvatarInitials({
     backgroundColor,
@@ -16,25 +17,30 @@ function AvatarInitials({
 
  const styles = StyleSheet.create({
     placeholderText: {
-     color: textColor,
+     color: textColor ? textColor : 'white',
     },
     placeholderContainer: {
      alignItems: 'center',
      justifyContent: 'center',
      backgroundColor: backgroundColor,
-     height: height,
-     width: width,
-     borderRadius: borderRadius,
+     height: height ? height : 35,
+     width: width ? width : 35,
+     borderRadius: borderRadius ? borderRadius : 50,
     },
     contactItemTitleStyle: {
-     fontSize: fontSize,
+     fontSize: fontSize ? fontSize : 18,
     },
  });
- const { familyName, givenName, company } = item || {};
- let displayName = `${familyName} ${givenName}`.trim();
- if (!familyName && !givenName) {
-  displayName = company || 'unknown';
+ const { familyName, givenName, company, firstName, lastName, companyName } = item || {};
+ let displayNameEDO = `${familyName} ${givenName}`.trim();
+ let displayNameTalk = `${firstName} ${lastName}`.trim();
+ if (APP_NAME === 'dindle' && !familyName && !givenName) {
+   displayNameEDO = company || 'unknown';
+ }else if(APP_NAME === 'talk' && !firstName && !lastName){
+   displayNameTalk = companyName || 'unknown';
  }
+
+ let displayName = APP_NAME === 'dindle' ? displayNameEDO : displayNameTalk;
  const getAvatarInitials = (textString) => {
  let rgx = new RegExp(/(\p{L}{1})\p{L}+/, 'gu');
  let match;
