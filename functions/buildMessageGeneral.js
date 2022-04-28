e5e294882d5db25cc445b099464dc8ad50143b67
Fabrizio Corpora <fabrizio.corpora@gmail.com>
2022-04-28 18:05:41 +0200

    color for symbol

--- functions/buildMessageGeneral.js
+++ functions/buildMessageGeneral.js
@@ -39,231 +39,256 @@ const removeNonUtf8 = (characters) => {
  * @returns
  */
  export const buildMessageGeneral = ({ message, styles, active = true }) => {
-    let outputArr = [];
-    let outputArrString = [];
-    let i = 0;
-    let text = message;
-    let openSelected = false;
-    let tmpMention = '';
-    let separator = { open: '"', close: '"' };
-    let re = new RegExp(regex.charactes);
-    let reEmoji = new RegExp(regex.emoji);
-    let accentsCharacte = new RegExp(regex.accentsCharactes);
-    let symbol = new RegExp(regex.symbols);
-    let unrecognized = new RegExp(regex.unrecognized);
+  let outputArr = [];
+  let outputArrString = [];
+  let i = 0;
+  let text = message;
+  let openSelected = false;
+  let tmpMention = '';
+  let separator = { open: '"', close: '"' };
+  let re = new RegExp(regex.charactes);
+  let reEmoji = new RegExp(regex.emoji);
+  let accentsCharacte = new RegExp(regex.accentsCharactes);
+  let symbol = new RegExp(regex.symbols);
+  let unrecognized = new RegExp(regex.unrecognized);
 
-    let openProgress = false;
-    let tmpProgress = '';
-    let openBudget = false;
-    let tmpBudget = '';
-    let openEffort = false;
-    let tmpEffort = '';
-    let openPriority = false;
-    let tmpPriority = '';
-    let openETA = false;
-    let tmpETA= '';
+  let openProgress = false;
+  let tmpProgress = '';
+  let idxProgress = 0;
+  let openBudget = false;
+  let tmpBudget = '';
+  let idxBudget = 0;
+  let openEffort = false;
+  let idxEffort = 0;
+  let tmpEffort = '';
+  let openPriority = false;
+  let idxPriority = 0;
+  let tmpPriority = '';
+  let openETA = false;
+  let tmpETA= '';
+  let idxETA = 0;
 
-    let openHashtag = false;
-    let tmpHashtag= '';
+  let openHashtag = false;
+  let tmpHashtag= '';
 
-    if (text) {
-      while (i < text.length) {
-        const isEmoj = reEmoji.test(text[i]+text[i+1]);
-        const isAccents = accentsCharacte.test(text[i]);
-        const isSymbols = symbol.test(text[i]);
-        const isUnrecognized = unrecognized.test(text[i]);
-        let writeEmail = false;
-        if (active) {
-          if (!openSelected && !openEffort && !openBudget && !openProgress && !openPriority && !openETA && !openHashtag){
-            //normal
-            if (text[i] !== '@' && text[i] !== '#' && text[i + 1] !== separator.open) {
-              if (isEmoj){
-                const emoji = emojiSplit(text[i]+text[i + 1]);
-                outputArr.push(emoji);
-                outputArrString.push(emoji);
-              } else if (!isUnrecognized && !isAccents && !isSymbols) {
-                removeNonUtf8(text[i]);
-                outputArr.push(removeNonUtf8(text[i]));
-                outputArrString.push(removeNonUtf8(text[i]));
-              } else if (isAccents || isSymbols) {
-                outputArr.push(text[i]);
-                outputArrString.push(text[i]);
-              }
-            } //email
-            else if (text[i] === '@' && text[i - 1] && re.test(text[i - 1])) {
+  if (text) {
+    while (i < text.length) {
+      const isEmoj = reEmoji.test(text[i]+text[i+1]);
+      const isAccents = accentsCharacte.test(text[i]);
+      const isSymbols = symbol.test(text[i]);
+      const isUnrecognized = unrecognized.test(text[i]);
+      let writeEmail = false;
+      if (active) {
+        if (!openSelected && !openEffort && !openBudget && !openProgress && !openPriority && !openETA && !openHashtag){
+          //normal
+          if (text[i] !== '@' && text[i] !== '#' && text[i + 1] !== separator.open) {
+            if (isEmoj){
+              const emoji = emojiSplit(text[i]+text[i + 1]);
+              outputArr.push(emoji);
+              outputArrString.push(emoji);
+            } else if (!isUnrecognized && !isAccents && !isSymbols) {
+              removeNonUtf8(text[i]);
+              outputArr.push(removeNonUtf8(text[i]));
+              outputArrString.push(removeNonUtf8(text[i]));
+            } else if (isAccents || isSymbols) {
               outputArr.push(text[i]);
               outputArrString.push(text[i]);
-              writeEmail = true;
             }
-            //add for open search
-            if (!writeEmail && text[i] === '@' && text[i + 1] !== separator.open) {
-              outputArr.push(text[i], separator.open, " ");
-              outputArrString.push(text[i], separator.open, " ");
-            }
-            if (text[i] === '#' && text[i + 1] !== separator.open) {
-              outputArr.push(text[i], separator.open, " ");
-              outputArrString.push(text[i], separator.open, " ");
-            }
+          } //email
+          else if (text[i] === '@' && text[i - 1] && re.test(text[i - 1])) {
+            outputArr.push(text[i]);
+            outputArrString.push(text[i]);
+            writeEmail = true;
           }
-        if (text[i] === '@' && text[i + 1] === separator.open) {
-          openSelected = true;
-        }
-        if (text[i] === '#' && text[i + 1] === separator.open) {
-          openHashtag = true;
-        }
-        //add for effort
-        if(text[i] === '~'){
-          openEffort = true;
-        }
-        //add for ETA
-        if(text[i] === 'A' && text[i-1] === 'T' && text[i-2] === 'E'){
-          openETA = true;
-        }
-        //add for budget
-        if(text[i] === '$'){
-          openBudget = true;
-        }
-
-        //add for priority
-        if(text[i] === '['){
-          openPriority = true;
-        }
-
-        //add for progress
-        if(text[i] === '%'){
-          openProgress = true;
-        }
-  
-
-        //effort build
-        if (openEffort) {
-          if(text[i] !== '~'){
-            tmpEffort = tmpEffort + text[i];
+          //add for open search
+          if (!writeEmail && text[i] === '@' && text[i + 1] !== separator.open) {
+            outputArr.push(text[i], separator.open, " ");
+            outputArrString.push(text[i], separator.open, " ");
           }
-          if (text[i] === ' '){
-            openEffort = false;
-            let effort = (
-              <Text key={i} style={styles.text}>
-                {tmpEffort}
-              </Text>
-            );
-            outputArr.push(effort);
-            outputArrString.push(tmpEffort);
-            tmpEffort = '';
+          if (text[i] === '#' && text[i + 1] !== separator.open) {
+            outputArr.push(text[i], separator.open, " ");
+            outputArrString.push(text[i], separator.open, " ");
           }
         }
-
-         //ETA build
-         if (openETA) {
-          if(text[i] !== 'A' && text[i-1] !== 'T' && text[i-2] !== 'E'){
-            tmpETA = tmpETA + text[i];
-          }
-          if (text[i] === ' '){
-            openETA = false;
-            let ETA = (
-              <Text key={i} style={styles.text}>
-                {tmpETA}
-              </Text>
-            );
-            outputArr.push(ETA);
-            outputArrString.push(tmpETA);
-            tmpETA = '';
-          }
-        }
-
-         //effort priority
-         if (openPriority) {
-          if(text[i] !== '['){
-            tmpPriority = tmpPriority + text[i];
-          }
-           if (text[i] === ']'){
-            openPriority = false;
-            let priority = (
-              <Text key={i} style={styles.text}>
-                {tmpPriority}
-              </Text>
-            );
-            outputArr.push(priority);
-            outputArrString.push(tmpPriority);
-            tmpPriority = '';
-        } 
+      if (text[i] === '@' && text[i + 1] === separator.open) {
+        openSelected = true;
+      }
+      if (text[i] === '#' && text[i + 1] === separator.open) {
+        openHashtag = true;
+      }
+      //add for effort
+      if(text[i] === '~'){
+        idxEffort = i;
+        openEffort = true;
+      }
+      //add for ETA
+      if(text[i] === 'A' && text[i-1] === 'T' && text[i-2] === 'E'){
+        idxETA = i-2;
+        openETA = true;
+      }
+      //add for budget
+      if(text[i] === '$'){
+        idxBudget = i;
+        openBudget = true;
       }
 
-        //budget build
-        if (openBudget) {
-          if(text[i] !== '$'){
-            tmpBudget = tmpBudget + text[i];
-          }
-          if (text[i] === ' '){
-            openBudget = false;
-            let budget = (
-              <Text key={i} style={styles.text}>
-                {tmpBudget}
-              </Text>
-            );
-            outputArr.push(budget);
-            outputArrString.push(tmpBudget);
-            tmpBudget = '';
-          }
-        }
-
-        //progress build
-        if (openProgress) {
-          if(text[i] !== '%'){
-            tmpProgress = tmpProgress + text[i];
-          }
-          if (text[i] === ' '){
-            openProgress = false;
-            let progress = (
-              <Text key={i} style={styles.text}>
-                {tmpProgress}
-              </Text>
-            );
-            outputArr.push(progress);
-            outputArrString.push(tmpProgress);
-            tmpProgress = '';
-          }
-        }
-        if (openHashtag) {
-          tmpHashtag = tmpHashtag + text[i];
-          if (text[i] ===  separator.close && text[i - 1] !== '#') {
-            openSelected = false;
-            let hashtag = (
-              <Text key={i} style={styles.text}>
-                {tmpHashtag}
-              </Text>
-            );
-            outputArr.push(hashtag);
-            outputArrString.push(tmpHashtag);
-            tmpHashtag = '';
-          }
-        }
-
-        if (openSelected) {
-          tmpMention = tmpMention + text[i];
-          if (text[i] ===  separator.close && text[i - 1] !== '@') {
-            openSelected = false;
-            let mention = (
-              <Text key={i} style={styles.text}>
-                {tmpMention}
-              </Text>
-            );
-            outputArr.push(mention);
-            outputArrString.push(tmpMention);
-            tmpMention = '';
-          }
-        }
-      } else {
-        outputArr.push(text[i]);
-        outputArrString.push(text[i]);
+      //add for priority
+      if(text[i] === '['){
+        idxPriority = i;
+        openPriority = true;
       }
-      i++;
+
+      //add for progress
+      if(text[i] === '%'){
+        idxProgress = i;
+        openProgress = true;
+      }
+
+
+      //effort build
+      if (openEffort) {
+        if(text[i] !== '~'){
+          tmpEffort = tmpEffort + text[i];
+        }
+        if (text[i] === ' '){
+          openEffort = false;
+          outputArr.splice(idxEffort, 1);
+          outputArrString.splice(idxEffort, 1);
+          let effort = (
+            <Text key={i} style={styles.text}>
+              ~{tmpEffort}
+            </Text>
+          );
+          outputArr.push(effort);
+          outputArrString.push(tmpEffort);
+          tmpEffort = '';
+          idxEffort = 0;
+        }
+      }
+
+       //ETA build
+       if (openETA) {
+        if(text[i] !== 'A' && text[i-1] !== 'T' && text[i-2] !== 'E'){
+          tmpETA = tmpETA + text[i];
+        }
+        if (text[i] === ' '){
+          openETA = false;
+          outputArr.splice(idxETA, 3);
+          outputArrString.splice(idxETA, 3);
+          let ETA = (
+            <Text key={i} style={styles.text}>
+              ETA{tmpETA}
+            </Text>
+          );
+          outputArr.push(ETA);
+          outputArrString.push(tmpETA);
+          tmpETA = '';
+          idxETA = 0;
+        }
+      }
+
+       //effort priority
+       if (openPriority) {
+        if(text[i] !== '['){
+          tmpPriority = tmpPriority + text[i];
+        }
+         if (text[i] === ']'){
+          openPriority = false;
+          outputArr.splice(idxPriority, 1);
+          outputArrString.splice(idxPriority, 1);
+          let priority = (
+            <Text key={i} style={styles.text}>
+              [{tmpPriority}
+            </Text>
+          );
+          outputArr.push(priority);
+          outputArrString.push(tmpPriority);
+          tmpPriority = '';
+          idxPriority= 0;
+      } 
     }
+
+      //budget build
+      if (openBudget) {
+        if(text[i] !== '$'){
+          tmpBudget = tmpBudget + text[i];
+        }
+        if (text[i] === ' '){
+          openBudget = false;
+          outputArr.splice(idxBudget, 1);
+          outputArrString.splice(idxBudget, 1);
+          let budget = (
+            <Text key={i} style={styles.text}>
+              ${tmpBudget}
+            </Text>
+          );
+          outputArr.push(budget);
+          outputArrString.push(tmpBudget);
+          tmpBudget = '';
+          idxBudget = 0;
+        }
+      }
+
+      //progress build
+      if (openProgress) {
+        if(text[i] !== '%'){
+          tmpProgress = tmpProgress + text[i];
+        }
+        if (text[i] === ' '){
+          openProgress = false;
+          outputArr.slice(idxProgress, 1);
+          outputArrString.slice(idxProgress, 1);
+          let progress = (
+            <Text key={i} style={styles.text}>
+              %{tmpProgress}
+            </Text>
+          );
+          outputArr.push(progress);
+          outputArrString.push(tmpProgress);
+          tmpProgress = '';
+          idxProgress = 0;
+        }
+      }
+      if (openHashtag) {
+        tmpHashtag = tmpHashtag + text[i];
+        if (text[i] ===  separator.close && text[i - 1] !== '#') {
+          openSelected = false;
+          let hashtag = (
+            <Text key={i} style={styles.text}>
+              {tmpHashtag}
+            </Text>
+          );
+          outputArr.push(hashtag);
+          outputArrString.push(tmpHashtag);
+          tmpHashtag = '';
+        }
+      }
+
+      if (openSelected) {
+        tmpMention = tmpMention + text[i];
+        if (text[i] ===  separator.close && text[i - 1] !== '@') {
+          openSelected = false;
+          let mention = (
+            <Text key={i} style={styles.text}>
+              {tmpMention}
+            </Text>
+          );
+          outputArr.push(mention);
+          outputArrString.push(tmpMention);
+          tmpMention = '';
+        }
+      }
+    } else {
+      outputArr.push(text[i]);
+      outputArrString.push(text[i]);
+    }
+    i++;
   }
-  return {
-    outputArr,
-    outputArrString,
-  };
+}
+return {
+  outputArr,
+  outputArrString,
+};
 };
 
 const handlerOpenUrl = (url) => {
